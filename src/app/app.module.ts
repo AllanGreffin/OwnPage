ee59593b2d4364d8aef078d1da1a1b0fb47a91c8
Allan <allan.greffin@k2sistemas.com.br>
2020-07-06 15:58:58 -0300

    adicao do bootstrap e dos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CurriculoComponent } from './curriculo/curriculo.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurriculoComponent,
+    SobreComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
